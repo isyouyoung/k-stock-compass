6047156699f94af444a6cff34776e9d4881b08d5
isyouyoung <isyouyoung@gmail.com>
2026-04-09 13:38:44 +0900

    SecurityConfig 생성

---
+++ src/main/java/kopo/kstockcompass/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package kopo.kstockcompass.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보안 끄기 (Postman 테스트를 위해 필수)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 모든 요청을 인증 없이 허용
+                );
+
+        return http.build();
+    }
+}
