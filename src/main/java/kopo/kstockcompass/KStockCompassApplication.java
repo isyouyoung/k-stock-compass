88a8099154c6ef5081adb51f0de582c80eb27832
isyouyoung <isyouyoung@gmail.com>
2026-04-09 11:16:59 +0900

    유저 인포 및 gitignore 작성

--- src/main/java/kopo/kstockcompass/KStockCompassApplication.java
+++ src/main/java/kopo/kstockcompass/KStockCompassApplication.java
@@ -2,10 +2,8 @@ package kopo.kstockcompass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // 1. 이 줄 추가
 
-// 2. exclude 설정을 추가해서 DB 연결 체크를 잠시 미룹니다. 추후 반드시 삭제할것!!!
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class KStockCompassApplication {
 
     public static void main(String[] args) {
--- src/main/java/kopo/kstockcompass/entity/UserInfo.java
+++ src/main/java/kopo/kstockcompass/entity/UserInfo.java
@@ -1,4 +1,32 @@
 package kopo.kstockcompass.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "USER_INFO")
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserInfo {
+
+    @Id
+    @Column(name = "USER_EMAIL", length = 100, nullable = false)
+    private String userEmail;
+
+    @Column(name = "USER_PWD", length = 255, nullable = false)
+    private String userPwd;
+
+    @Column(name = "USER_NAME", length = 50, nullable = false)
+    private String userName;
+
+    @Column(name = "USER_PNUM", length = 20, nullable = false, unique = true)
+    private String userPnum;
+
+    @Column(name = "REG_DT", nullable = false)
+    private LocalDateTime regDt;
 }
