adce27b849bced179e47c592e09b5b934999a103
isyouyoung <isyouyoung@gmail.com>
2026-04-10 15:01:55 +0900

    SecurityConfig 작성

--- src/main/java/kopo/kstockcompass/config/SecurityConfig.java
+++ src/main/java/kopo/kstockcompass/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,5 +28,11 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    // PasswordEncoder Bean 등록
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
