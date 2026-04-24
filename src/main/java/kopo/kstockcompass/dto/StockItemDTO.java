348e7b7519300c867f0a8fb134882a654c3bd388
isyouyoung <isyouyoung@gmail.com>
2026-04-24 10:01:51 +0900

    공공데이터 API 응답에서 필요한 필드만 담는 DTO

---
+++ src/main/java/kopo/kstockcompass/dto/StockItemDTO.java
@@ -0,0 +1,14 @@
+package kopo.kstockcompass.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class StockItemDTO {
+    private String srtnCd;   // 종목코드
+    private String itmsNm;   // 종목명
+    private String clpr;     // 종가
+    private String fltRt;    // 등락률
+    private String vs;       // 전일대비
+}
