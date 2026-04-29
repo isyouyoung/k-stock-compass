a58dd48c79dbdec9f746a72ca463d35bae9ad021
isyouyoung <isyouyoung@gmail.com>
2026-04-29 14:06:28 +0900

    WebClient로 변경

--- src/main/java/kopo/kstockcompass/config/AppConfig.java
+++ src/main/java/kopo/kstockcompass/config/AppConfig.java
@@ -3,14 +3,14 @@ package kopo.kstockcompass.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient() {
+        return WebClient.builder().build();
     }
 
     @Bean
