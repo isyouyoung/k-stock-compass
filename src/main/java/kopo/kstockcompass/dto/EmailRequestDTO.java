38daad09009de2f313651a8f07716573e15c22e7
isyouyoung <isyouyoung@gmail.com>
2026-04-29 14:09:07 +0900

    이메일요청DTO 아이디 비밀번호 찾기

---
+++ src/main/java/kopo/kstockcompass/dto/EmailRequestDTO.java
@@ -0,0 +1,10 @@
+package kopo.kstockcompass.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class EmailRequestDTO {
+    private String userEmail;
+}
