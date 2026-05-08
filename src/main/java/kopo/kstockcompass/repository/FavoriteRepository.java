c0b6a9ca32d1afb52679b22816e488c020188f99
isyouyoung <isyouyoung@gmail.com>
2026-05-08 12:54:40 +0900

    관심종목 삭제 조회 구현

--- src/main/java/kopo/kstockcompass/repository/FavoriteRepository.java
+++ src/main/java/kopo/kstockcompass/repository/FavoriteRepository.java
@@ -1,7 +1,18 @@
 package kopo.kstockcompass.repository;
-
 import kopo.kstockcompass.repository.entity.FavoriteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FavoriteRepository extends JpaRepository<FavoriteEntity, Long> {
+
+    // 내 관심종목 목록 조회
+    List<FavoriteEntity> findByUserEmail(String userEmail);
+
+    // 특정 종목 관심 여부 확인 (중복 추가 방지)
+    Optional<FavoriteEntity> findByUserEmailAndStockCd(String userEmail, String stockCd);
+
+    // 관심종목 삭제
+    void deleteByUserEmailAndStockCd(String userEmail, String stockCd);
 }
