3567901e1ba5e323dca57081978927b2bd4de0e4
isyouyoung <isyouyoung@gmail.com>
2026-05-08 12:51:40 +0900

    더미 데이터 => 실제 데이터 변경

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -5,23 +5,9 @@ const state = {
     loggedIn: false,
     user: null,
     currentPage: 'login',
-    favorites: [
-        {code:'005930',name:'삼성전자',market:'KOSPI',price:61200,change:1200,pct:2.00},
-        {code:'000660',name:'SK하이닉스',market:'KOSPI',price:182500,change:-1500,pct:-0.81},
-        {code:'035420',name:'NAVER',market:'KOSPI',price:215000,change:3000,pct:1.41},
-        {code:'035720',name:'카카오',market:'KOSDAQ',price:45800,change:-200,pct:-0.43},
-    ],
-    alerts: [
-        {id:1,code:'005930',name:'삼성전자',target:70000,direction:'이상',current:61200},
-        {id:2,code:'000660',name:'SK하이닉스',target:200000,direction:'이상',current:182500},
-        {id:3,code:'035420',name:'NAVER',target:180000,direction:'이하',current:215000},
-    ],
-    alertLogs: [
-        {id:1,name:'삼성전자',msg:'목표가 70,000원 이상 도달!',time:'2026.03.24 14:32',read:false},
-        {id:2,name:'SK하이닉스',msg:'목표가 200,000원 이상 도달!',time:'2026.03.23 09:15',read:false},
-        {id:3,name:'NAVER',msg:'목표가 180,000원 이하 도달!',time:'2026.03.22 15:48',read:true},
-        {id:4,name:'카카오',msg:'목표가 50,000원 이상 도달!',time:'2026.03.21 11:20',read:true},
-    ],
+    favorites: [],
+    alerts: [],
+    alertLogs: [],
     currentStock: null,
     editAlertId: null,
     delFavCode: null,
@@ -72,6 +58,7 @@ function navigate(page, params={}) {
     if(savedToken && savedEmail){
         state.loggedIn = true;
         state.user = { email: savedEmail, nickname: savedEmail.split('@')[0] };
+        loadFavorites().then(() => navigate('main'));
+    } else {
+        navigate('main');
     }
-
-    navigate('main');
