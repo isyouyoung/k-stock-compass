c7c092bcd7672d062999ae789a691bc584b95e5b
isyouyoung <isyouyoung@gmail.com>
2026-05-11 10:31:21 +0900

    알림 종목 구현

---
+++ src/main/java/kopo/kstockcompass/dto/AlertDTO.java
@@ -0,0 +1,22 @@
+package kopo.kstockcompass.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlertDTO {
+
+    private Long alertId;
+    private String userEmail;
+    private String stockCd;
+    private String stockNm;
+    private BigDecimal targetPrice;
+    private String regDt;
+}
