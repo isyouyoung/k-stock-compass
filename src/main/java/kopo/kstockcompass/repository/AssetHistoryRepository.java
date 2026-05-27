1914b531ada60ec18220aaac1488831ffef15649
isyouyoung <isyouyoung@gmail.com>
2026-05-27 15:42:13 +0900

    계좌 변동성 그래프 기능추가 (레포지토리)

---
+++ src/main/java/kopo/kstockcompass/repository/AssetHistoryRepository.java
@@ -0,0 +1,10 @@
+package kopo.kstockcompass.repository;
+
+import kopo.kstockcompass.repository.entity.AssetHistoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AssetHistoryRepository extends JpaRepository<AssetHistoryEntity, Long> {
+    List<AssetHistoryEntity> findByUserEmailOrderByRegDtAsc(String userEmail);
+}
