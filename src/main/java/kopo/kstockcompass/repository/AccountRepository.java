c114347ee0b2852459655ff4cad72577417693ed
isyouyoung <isyouyoung@gmail.com>
2026-06-01 11:00:29 +0900

    회원탈퇴 기능 추가중

--- src/main/java/kopo/kstockcompass/repository/AccountRepository.java
+++ src/main/java/kopo/kstockcompass/repository/AccountRepository.java
@@ -4,4 +4,6 @@ import kopo.kstockcompass.repository.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<AccountEntity, String> {
+
+    void deleteByUserEmail(String userEmail); // PK가 userEmail이라 deleteById도 가능
 }
