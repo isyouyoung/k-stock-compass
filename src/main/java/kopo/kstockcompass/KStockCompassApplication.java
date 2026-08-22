a87ae8901c29f0c7dc7c152a7736d8de21df7b3d
isyouyoung <isyouyoung@gmail.com>
2026-08-23 00:09:15 +0900

    게시판 작성글 시간 한국시간으로 수정

--- src/main/java/kopo/kstockcompass/KStockCompassApplication.java
+++ src/main/java/kopo/kstockcompass/KStockCompassApplication.java
@@ -1,12 +1,21 @@
 package kopo.kstockcompass;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class KStockCompassApplication {
+
+    // 서버 구동 시 JVM 타임존을 한국 시간(KST)으로 강제 고정
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KStockCompassApplication.class, args);
